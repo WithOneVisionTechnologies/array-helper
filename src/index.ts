29bf0796dcb7029df6829799763e6352182f8f7a
Doug Shontz, Jr. <doug@shontz.net>
2024-12-05 11:25:17 -0500

    feat(orderBy): Removing redundant type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-/*** Limit order by directions */
-export type orderByDirection = "asc" | "desc";
-
 /** Encapsulating class for all array-helpers.  All methods
  * below this are static so there does not need to be any
  * references to constructors.  This class is meant to be
@@ -21,7 +18,7 @@ export default class ArrayHelper {
    }
 
    /*** Take in an array of keys and orders and re-order the entire array based on that input. */
-   public static orderBy<T>(arr: T[], keys: string[], directions: orderByDirection[]): T[] {
+   public static orderBy<T>(arr: T[], keys: string[], directions: ("asc" | "desc")[]): T[] {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       return arr.sort((a: any, b: any) => {
          let i = 0;
